d329f349fbb40b03f6d8aad52072a802ee200bda
sreelakshmii-a <sreelakshmia1723@gmail.com>
2025-07-04 20:45:09 +0530

    First step Finished

--- Attempt1.c
+++ Attempt1.c
@@ -1,32 +1,89 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-void arrangement(char A[]) {
-    char P[8], Q[8], R[8]; 
+char* A[50] = {
+        "Ball", "Light", "Spoon", "Chair", "Laptop", "Bottle", "Book",
+        "Phone", "Pillow", "Pen", "Backpack", "Headphones", "Table", "Mirror",
+        "Blanket", "Plant", "Clock", "Fan", "Camera", "Cup", "Key"
+    };
 
-    for (int i = 0; i < 7; i++) {
-        P[i] = A[i];
-        Q[i] = A[i + 1];
-        R[i] = A[i + 2];
-    }
+  void waitForUser() {
+    printf("\nPress Enter to continue...");
+    getchar(); // for the newline from previous input
+    getchar(); // to wait for actual Enter
+}
 
-    P[7] = '\0';
-    Q[7] = '\0';
-    R[7] = '\0';
 
-    printf("Pile 1\n %s", P);
-    printf("Pile 2\n %s", Q);
-    printf("Pile 3\n %s", R);
+void arrangement(char* A[],char* P[],char* Q[],char* R[]) {
+  int p=0,q=0,r=0;
+  for(int i=0;i<21;i++){
+    if(i%3==0)
+      R[r++]=A[i];
+    else if (i%3==1)
+      Q[q++]=A[i];
+    else
+      P[p++]=A[i];
+    
+  }    
+
+   printf("\nPile 1:\n");
+    for (int i = 0; i < 7; i++) printf("%s\t", P[i]);
+    printf("\nPile 2:\n");
+    for (int i = 0; i < 7; i++) printf("%s\t", Q[i]);
+    printf("\nPile 3:\n");
+    for (int i = 0; i < 7; i++) printf("%s\t", R[i]);
+
+  
+}
+
+
+char PileSelect(char* A[],char* P[],char* Q[],char* R[],int n){
+ int index=0;
+  if(n==1){
+    for(int i=0;i<7;i++) A[index++]=Q[i];
+    for (int i = 0; i < 7; i++) A[index++] = P[i];
+    for (int i = 0; i < 7; i++) A[index++] = R[i];
+    
+  }
+  else if(n==2){
+     for (int i = 0; i < 7; i++) A[index++] = P[i];
+    for (int i = 0; i < 7; i++) A[index++] = Q[i];
+      for (int i = 0; i < 7; i++) A[index++] = R[i];
+    
+  }
+  else if (n== 3){
+     
+    for (int i = 0; i < 7; i++) A[index++] = P[i];
+    for (int i = 0; i < 7; i++) A[index++] = R[i];
+    for (int i = 0; i < 7; i++) A[index++] = Q[i];
+  }
+
+  
 }
 
 
 
-
 void main(){
-  printf("Ball, Light, Spoon, Chair, Laptop, Bottle, Book,\n Phone, Pillow, Pen, Backpack, Headphones, Table, Mirror,\n Blanket, Plant, Clock, Fan, Camera, Cup, Key\n");
-  printf("Select something from the above collection and keep it a secret!\n");
-  for(int i=0;i<3;i++){
+  int n;
+  char* P[50], *Q[50], *R[50];
+  
+  printf("Remember anything from the following\n");
+  for (int i = 0; i < 21; i++) {
+        printf("%2d. %s\n", i + 1, A[i]);
+    }
 
-  }
+  waitForUser();
+  for (int round = 0; round < 3; round++) {
+        arrangement(A, P, Q, R);
+        printf("\nIn which pile is your item? (1, 2, or 3): ");
+        scanf("%d", &n);
+        PileSelect(A, P, Q, R, n);
+    }
+  printf("\n\n");
+
+  printf("%s!!!\n",A[10]);
+  
 
 }
